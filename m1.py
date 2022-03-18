3a5ff1d39b2c442b91827a5afa31b73ae16c57e7
King <king03officiall>
2022-03-18 09:38:51 +0500

    m1

---
+++ m1.py
@@ -0,0 +1,22 @@
+
+def Sum(x):
+    a=0
+    for i in x[::-1]:
+        a=a*10 + i
+    print(a)
+    return a
+def bolish(a,b):
+    x=Sum(a)+Sum(b)
+    print(x)
+    k=[]
+    while(x):
+        k.append(x%10)
+        x//=10
+    print(k)
+    return k
+l1=[5,2]
+l2=[5,1]
+print(bolish(l1, l2))
+print(-23<-22)
+#1 opp
+#2 
